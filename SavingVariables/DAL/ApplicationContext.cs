27e4085deebb3f013ca3977fa22f362df373f0c2
Bradley Guthrie <guppy96@gmail.com>
2016-09-16 19:10:49 -0500

    DAL, Migrations classes created

---
+++ SavingVariables/DAL/ApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using SavingVariables.Models;
+
+namespace SavingVariables.DAL
+{
+    class ApplicationContext :DbContext
+    {
+        public virtual DbSet<Constants> Constants { get; set; }
+
+    }
+}
