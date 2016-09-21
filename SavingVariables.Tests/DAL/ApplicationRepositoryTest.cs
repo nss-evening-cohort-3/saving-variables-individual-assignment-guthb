4afc93c4852d5ae31d08442af631afdcec3644ed
Bradley Guthrie <guppy96@gmail.com>
2016-09-20 20:39:13 -0500

    mock built

--- SavingVariables.Tests/DAL/ApplicationRepositoryTest.cs
+++ SavingVariables.Tests/DAL/ApplicationRepositoryTest.cs
@@ -16,7 +16,28 @@ namespace SavingVariables.Tests.DAL
         Mock<ApplicationContext> mock_context { get; set; }
         Mock<DbSet<Constants>> mock_constants_table { get; set; }
         List<Constants> constants_list { get; set; } //this is my fake database
-        
+        ApplicationRepository repo { get; set; }
+
+        //create method for Mock Datastore
+        public void ConnectMocksToDatastore()
+        {
+            var queryable_list = constants_list.AsQueryable();
+
+            //These lines trick LINQ, impersonates a Database table
+            mock_constants_table.As<IQueryable<Constants>>().Setup(m => m.Provider).Returns(queryable_list.Provider);
+            mock_constants_table.As<IQueryable<Constants>>().Setup(m => m.Expression).Returns(queryable_list.Expression);
+            mock_constants_table.As<IQueryable<Constants>>().Setup(m => m.GetEnumerator()).Returns(() => queryable_list.GetEnumerator());
+
+            //Constants property will return fake database table
+            mock_context.Setup(c => c.Constants).Returns(mock_constants_table.Object);
+
+            //Constants property will retrun fake database table
+            mock_constants_table.Setup(t => t.Add(It.IsAny<Constants>())).Callback((Constants c) => constants_list.Add(c));
+            mock_constants_table.Setup(t => t.Remove(It.IsAny<Constants>())).Callback((Constants c) => constants_list.Remove(c));
+
+        }
+
+
 
         [TestMethod]
         public void EnsureCanCreateRepoInstance()
@@ -37,9 +58,21 @@ namespace SavingVariables.Tests.DAL
 
 
         [TestMethod]
-        public void EnsureCanAddToRepoInstance()
+        public void EnsureCanAddConstantsToDatabase()
         {
+            
+            //this is my first test that will require faking the databaser
+            //Arrange
+            ApplicationRepository repo = new ApplicationRepository();
+            ConnectMocksToDatastore();
 
+
+            Constants my_contstant  = new Constants { ConstantName = 'a', Varible = 1};
+
+            //Act
+           
+
+            //Assert
     
         }
 
